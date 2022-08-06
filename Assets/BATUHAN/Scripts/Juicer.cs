4d227e13df39446de44f720106a89e4a4e3d061a
fbatuhanr <fbatuhanr@gmail.com>
2022-08-06 11:27:05 +0300

    Update Juicer.cs

--- Assets/BATUHAN/Scripts/Juicer.cs
+++ Assets/BATUHAN/Scripts/Juicer.cs
@@ -1,7 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
+using EMRE.Scripts;
 using UnityEngine;
 
 public class Juicer : MonoBehaviour
 {
+    [SerializeField] private Transform juiceOutPoint;
+    
+    Queue<Fruit> fruits = new Queue<Fruit>();
+
+    private void EnqueuItem(Fruit fruit)
+    {
+        fruits.Enqueue(fruit);
+    }
+    private void DequeuItem()
+    {
+        var fruit = fruits.Dequeue();
+        var juice = fruit.TurnToJuice();
+
+        juice.transform.position = juiceOutPoint.position;
+
+        var pos = new Vector3(0, 0, 10);
+        fruit.Throw(pos);
+    }
+    private void JuiceIt()
+    {
+        
+    }
 }
