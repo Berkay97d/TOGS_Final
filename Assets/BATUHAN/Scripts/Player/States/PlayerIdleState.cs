fba8f377247338305182093e8ac77e5a1555a506
fbatuhanr <fbatuhanr@gmail.com>
2022-08-07 11:52:38 +0300

    Update PlayerIdleState.cs

--- Assets/BATUHAN/Scripts/Player/States/PlayerIdleState.cs
+++ Assets/BATUHAN/Scripts/Player/States/PlayerIdleState.cs
@@ -13,12 +13,8 @@ public class PlayerIdleState : PlayerBaseState
     public override void UpdateState(PlayerStateManager player)
     {
         if (Input.touchCount > 0 && !CinemachineController.isTutorialCamActive)
-        {
             player.SwitchState(player.runState);
-        }
         else
-        {
             player._playerMovement.rigidbody.velocity = Vector3.zero;
-        }
     }
 }
