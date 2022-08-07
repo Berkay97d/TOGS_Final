cdc7b912fc5b292640f77236031d34806b73083a
Emre Berat Kıran <doctoremre7@gmail.com>
2022-08-07 10:35:57 +0300

    Update CinemachineController.cs

--- Assets/BATUHAN/Scripts/CinemachineController.cs
+++ Assets/BATUHAN/Scripts/CinemachineController.cs
@@ -1,9 +1,5 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using Cinemachine;
 using Helpers;
-using UnityEditor.Timeline.Actions;
 using UnityEngine;
 
 public class CinemachineController : Scenegleton<CinemachineController>
